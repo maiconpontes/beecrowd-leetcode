0d84f3bb81267920f6aced3856113aeeb04d880b
Maicon Pontes <116598186+maiconpontes@users.noreply.github.com>
2023-04-03 19:16:57 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+//template for beecrowd
+
+using namespace std;
+
+int main()
+{
+    int a=0;
+    cin >> a;
+    
+    for (int i=0;i<a;i++){
+        
+    }
+
+    return 0;
+}
